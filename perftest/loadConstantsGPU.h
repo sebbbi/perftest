c45ad1d0a5f2eb06321e845b7a6e4b2d40fbf95d
sebbbi <sebastian.aaltonen@gmail.com>
2016-12-21 15:29:58 +0200

    cbuffer header

---
+++ perftest/loadConstantsGPU.h
@@ -0,0 +1,13 @@
+// SHARED HEADER BETWEEN CPU AND GPU
+#ifdef __cplusplus
+#include "dataTypes.hpp"
+#endif
+
+struct LoadConstants
+{
+	uint numElements;
+	uint writeIndex;		// Always 0xffffffff (= never write). But the compiler doesn't know this :)
+
+	uint2 padding;
+};
+
